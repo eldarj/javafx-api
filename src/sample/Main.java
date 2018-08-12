607059a6c2c6df5a434748dba2da64768b1bc2d8
eldar.jah <eldar.jah@gmail.com>
2018-08-12 12:32:29 +0200

    Added sample login view.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,10 +10,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("views/login.fxml"));
 
         primaryStage.setTitle("Feedworks");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.show();
     }
 
